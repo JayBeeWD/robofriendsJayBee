07dd1b007ce63d96702bc9880882e0885555f2fd
JayBeeWD <bullisnancyjane@gmail.com>
2022-04-27 07:11:32 -0400

    fix errors in App.js

--- src/App.js
+++ src/App.js
@@ -2,52 +2,39 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
-//import './App.css';
-/*
-const state = {
-    robots: robots,
-    searchfield: ''
-}*/
-//const App = () => {
-    class App extends Component {
+import './App.css';
+
+class App extends Component {
         constructor() {
             super()
             this.state = {
-                robots: robots,//robots,
+                robots: [],
                 searchfield: ''
             }
-            //console.log('constructor');
         }
-/*
         componentDidMount() {
-            fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users}));
-            }
-           
-           // console.log('componentDidMount');
-        }*/
-
+            this.setState({ robots: robots});
+        }
+     
         onSearchChange = (event) => {
             this.setState({ searchfield: event.target.value })
-        //console.log(event.target.value);
-        //console.log(filteredRobots);
-        }
+  
+        } 
 
         render() {
             const filteredRobots = this.state.robots.filter(robots => {
-                return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-            })
-           // console.log('render');
-return (
+                return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()
+            )              
+            });
+                       
+return (  
     <div className="tc">
     <h1 className='f1'>RoboFriends</h1>
     <SearchBox searchChange={this.onSearchChange}/>
     <CardList robots={filteredRobots}/>
     </div>
-);
-        }}
-   
+)};
+}
+    
 export default App;
-
 
